e61065f1c885aba20e2c5253916f84b8050dbb83
Mykola Myslovskyi <adriandecita@gmail.com>
2021-12-11 19:06:46 +0200

    feat: add matchers, and slightly rework API to fit them in

---
+++ test/src/kuma/match.mjs
@@ -0,0 +1,94 @@
+import test from 'ava';
+import { parseMacroArgs, extractMacros } from '../../../lib/kuma/match.js';
+
+test('extractMacros should recognize macros without arguments', (t) => {
+  const input = '{{dummyMacro}}';
+  const result = extractMacros(input);
+  t.is(result.length, 1);
+  t.is(result[0].match, '{{dummyMacro}}');
+
+  t.timeout(200);
+});
+
+test("extractMacros's result should contain matched value", (t) => {
+  const input = '{{dummyMacro}}';
+  const result = extractMacros(input);
+  t.is(result[0].match, '{{dummyMacro}}');
+
+  t.timeout(200);
+});
+
+test("extractMacros's result should contain recognized function name", (t) => {
+  const input = '{{dummyMacro}}';
+  const result = extractMacros(input);
+  t.is(result[0].functionName, 'dummyMacro');
+
+  t.timeout(200);
+});
+
+test('extractMacros should recognize multiple macros, if present', (t) => {
+  const input = '{{dummyMacro1}}{{dummyMacro2}}';
+  const result = extractMacros(input);
+  t.is(result.length, 2);
+  t.is(result[0].match, '{{dummyMacro1}}');
+  t.is(result[1].match, '{{dummyMacro2}}');
+
+  t.timeout(200);
+});
+
+test('extractMacros should recognize macros with arguments', (t) => {
+  const input = '{{dummyMacro("Some", "args")}}';
+  const result = extractMacros(input);
+  t.is(result.length, 1);
+  t.is(result[0].args, '"Some", "args"');
+
+  t.timeout(200);
+});
+
+test('parseMacroArgs should recognize all arguments', (t) => {
+  const input = '"Some", "args", 0';
+  const result = parseMacroArgs(input);
+  t.is(result.length, 3);
+
+  t.timeout(200);
+});
+
+test('parseMacroArgs should recognize strings in gouble quotes', (t) => {
+  const input = '"Some", "args", 0';
+  const result = parseMacroArgs(input);
+  t.is(result.length, 3);
+  t.is(result[0], 'Some');
+  t.is(result[1], 'args');
+
+  t.timeout(200);
+});
+
+test('parseMacroArgs should recognize strings in single quotes', (t) => {
+  const input = "'Some', 'args', 0";
+  const result = parseMacroArgs(input);
+  t.is(result.length, 3);
+  t.is(result[0], 'Some');
+  t.is(result[1], 'args');
+
+  t.timeout(200);
+});
+
+test('parseMacroArgs should recognize strings with quotes in mixed style', (t) => {
+  const input = '\'Some\', "args", 0';
+  const result = parseMacroArgs(input);
+  t.is(result.length, 3);
+  t.is(result[0], 'Some');
+  t.is(result[1], 'args');
+
+  t.timeout(200);
+});
+
+test('parseMacroArgs should recognize integer arguments', (t) => {
+  const input = '\'Some\', "args", 0, 20';
+  const result = parseMacroArgs(input);
+  t.is(result.length, 4);
+  t.is(result[2], 0);
+  t.is(result[3], 20);
+
+  t.timeout(200);
+});
