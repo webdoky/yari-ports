815ce6e715e49ee551b67df7fccd6e7f509a233c
Mykola Myslovskyi <adriandecita@gmail.com>
2021-12-17 11:57:26 +0200

    chore: add some tests for jsxref macro

--- test/src/kuma/macros/jsxref.mjs
+++ test/src/kuma/macros/jsxref.mjs
@@ -7,3 +7,19 @@ test("Macros 'jsxref' should be present", (t) => {
 
   t.timeout(200);
 });
+
+test("Macros 'jsxref' should return a link with proper href and text caption", (t) => {
+  const kumaPorts = macros({ targetLocale: 'en-US' });
+  const jsxref = kumaPorts.lookup('JSxRef');
+  t.snapshot(jsxref('Operators/yield', 'yield'));
+
+  t.timeout(200);
+});
+
+test("Macros 'jsxref' should work on arguments with asterisk", (t) => {
+  const kumaPorts = macros({ targetLocale: 'en-US' });
+  const jsxref = kumaPorts.lookup('JSxRef');
+  t.snapshot(jsxref('Operators/yield*', 'yield*'));
+
+  t.timeout(200);
+});
