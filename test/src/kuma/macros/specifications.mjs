9bda3d90341997ac0a299681d7fda98864223643
Mykola Myslovskyi <adriandecita@gmail.com>
2021-12-21 00:48:30 +0200

    feat: add initial support for specifications macro

---
+++ test/src/kuma/macros/specifications.mjs
@@ -0,0 +1,19 @@
+import test from 'ava';
+import { macros } from '../../../../lib/kuma/index.js';
+
+test("Macros 'specifications' should be present", (t) => {
+  const kumaPorts = macros({});
+  t.truthy(kumaPorts.lookup('specifications'));
+});
+
+test("Macros 'specifications' should generate markup according to given query", (t) => {
+  const kumaPorts = macros({});
+  const specifications = kumaPorts.lookup('specifications');
+  t.snapshot(specifications('some.query'));
+});
+
+test("Macros 'specifications' should generate markup even without explicit query, if it is provided with the context", (t) => {
+  const kumaPorts = macros({ browserCompat: 'some.query' });
+  const specifications = kumaPorts.lookup('specifications');
+  t.snapshot(specifications());
+});
