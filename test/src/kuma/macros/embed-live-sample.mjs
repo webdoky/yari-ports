0820fd8be3cebf8a612355954248017f776635d8
Mykola Myslovskyi <adriandecita@gmail.com>
2022-02-12 23:33:36 +0200

    fix: disable embedLiveSample macro

--- test/src/kuma/macros/embed-live-sample.mjs
+++ test/src/kuma/macros/embed-live-sample.mjs
@@ -1,15 +1,15 @@
 import test from 'ava';
 import { macros } from '../../../../lib/kuma/index.js';
 
-test("Macros 'EmbedLiveSample' should be present", (t) => {
-  const kumaPorts = macros({});
-  t.truthy(kumaPorts.lookup('EmbedLiveSample'));
+// test("Macros 'EmbedLiveSample' should be present", (t) => {
+//   const kumaPorts = macros({});
+//   t.truthy(kumaPorts.lookup('EmbedLiveSample'));
 
-  t.timeout(200);
-});
+//   t.timeout(200);
+// });
 
-test("Macros 'EmbedLiveSample' should generate proper markup", (t) => {
-  const kumaPorts = macros({ path: '/uk/docs/Web/HTML/Element/input' });
-  const embedLiveSample = kumaPorts.lookup('EmbedLiveSample');
-  t.snapshot(embedLiveSample('examplebutton', 200, 55, '', '', 'nobutton'));
-});
+// test("Macros 'EmbedLiveSample' should generate proper markup", (t) => {
+//   const kumaPorts = macros({ path: '/uk/docs/Web/HTML/Element/input' });
+//   const embedLiveSample = kumaPorts.lookup('EmbedLiveSample');
+//   t.snapshot(embedLiveSample('examplebutton', 200, 55, '', '', 'nobutton'));
+// });
