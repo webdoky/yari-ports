e70b01ced19e7ae3aaa4f240a37e5b1141b17271
Mykola Myslovskyi <adriandecita@gmail.com>
2022-11-07 22:32:51 +0200

    feat: add callMacro function to macro container

---
+++ test/src/kuma/injectCallMacro.mjs
@@ -0,0 +1,51 @@
+import test from 'ava';
+import { injectCallMacro } from '../../../lib/kuma/index.js';
+
+test('The callMacro function in the context', (t) => {
+  const newContext = injectCallMacro({ smth: '???' });
+  t.truthy(newContext.callMacro);
+  t.timeout(200);
+});
+
+test('Old properties are preserved in the context', (t) => {
+  const initialContext = { smth: '???' };
+
+  const newContext = injectCallMacro(initialContext);
+  t.is(newContext.smth, initialContext.smth);
+
+  t.timeout(200);
+});
+
+test("callMacro function applies the given context in it's scope", (t) => {
+  const initialContext = { smth: '???' };
+
+  const newContext = injectCallMacro(initialContext);
+
+  function macro() {
+    t.is(this.smth, initialContext.smth);
+    t.truthy(this.callMacro);
+  }
+
+  newContext.callMacro(macro);
+
+  t.timeout(200);
+});
+
+test("callMacro function applies the given context in it's scope recursively", (t) => {
+  const initialContext = { smth: '???' };
+
+  const newContext = injectCallMacro(initialContext);
+
+  function innerMacro() {
+    t.is(this.smth, initialContext.smth);
+    t.truthy(this.callMacro);
+  }
+
+  function outerMacro() {
+    newContext.callMacro(innerMacro);
+  }
+
+  newContext.callMacro(outerMacro);
+
+  t.timeout(200);
+});
