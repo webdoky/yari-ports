daec1f362e920a1b828d14d545845eb25e98c693
Mykola Myslovskyi <adriandecita@gmail.com>
2023-08-07 00:55:13 +0300

    fix: bring jsRef macro up to date, get it working with page-type

--- test/src/kuma/macros/js-ref.mjs
+++ test/src/kuma/macros/js-ref.mjs
@@ -1,9 +1,28 @@
 import test from 'ava';
 
 import { macros } from '../../../../lib/kuma';
+import registryJs from '../../mocks/registry-js.mjs';
 
 test("Macros 'jsref' should be present", (t) => {
   t.truthy(macros({}).lookup('jsref'));
+
+  t.timeout(200);
+});
+
+// TODO: update this snapshot later, when all our pages have proper page-type
+test("Macros 'jsref' should generate complete navigation for standard object String", (t) => {
+  const jsref = macros({
+    env: {
+      targetLocale: 'uk',
+      slug: 'Web/JavaScript/Reference/Global_Objects/String/at',
+      path: '/uk/docs/Web/JavaScript/Reference/Global_Objects/String/at',
+    },
+    registry: {
+      getPagesData: () => registryJs,
+    },
+  }).lookup('jsref');
+
+  t.snapshot(jsref());
 
   t.timeout(200);
 });
