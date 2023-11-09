fc205d2efb8a1a4d2cf860e00774ffa4d896c5af
Mykola Myslovskyi <adriandecita@gmail.com>
2023-11-09 21:45:27 +0200

    feat: update CSS-ref macros to take into account page-type

--- test/src/kuma/macros/css-ref.mjs
+++ test/src/kuma/macros/css-ref.mjs
@@ -1,9 +1,27 @@
 import test from 'ava';
 
 import { macros } from '../../../../lib/kuma';
+import registryCss from '../../mocks/registry-css.mjs';
 
 test("Macros 'cssref' should be present", (t) => {
   t.truthy(macros({}).lookup('cssref'));
+
+  t.timeout(200);
+});
+
+test("Macros 'cssref' should generate complete navigation for given set of properties pages", (t) => {
+  const cssref = macros({
+    env: {
+      targetLocale: 'uk',
+      slug: 'Web/CSS/-webkit-border-before',
+      path: '/uk/docs/Web/CSS/-webkit-border-before',
+    },
+    registry: {
+      getPagesData: () => registryCss,
+    },
+  }).lookup('cssref');
+
+  t.snapshot(cssref());
 
   t.timeout(200);
 });
